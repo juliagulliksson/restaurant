8fd9d9e53b53bc42dff1c49d9abdaf890eb9e6f2
Caroline Wood <carowood@gmail.com>
2018-08-20 10:30:20 +0200

    Change file name to uppercase

--- src/components/SeatingForm.js
+++ src/components/SeatingForm.js
@@ -1,47 +1,53 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
 export default class SeatingForm extends Component {
-
   state = {
     firstSeating: false,
     secondSeating: false
-  }
+  };
 
   handleClick = () => {
-    this.setState((prevState) => ({
+    this.setState(prevState => ({
       firstSeating: true
     }));
-  }
+  };
 
-  render(){
+  render() {
     return (
       <React.Fragment>
-        { this.props.seatingTimes.seatingTwo > 14 && 
-        this.props.seatingTimes.seatingTwo > 14 ? 
-        <p>Sorry! No seating times available at this date</p>
-        : (
-      
-        <form>
-          <h2>Available seating times:</h2>
- 
-             <label>
-              <input type="radio" value="firstSeating" name="chosenSeating"
-               onChange={this.props.handleChange} />
-              18:00
-            </label> 
+        {this.props.seatingTimes.seatingTwo > 14 &&
+        this.props.seatingTimes.seatingTwo > 14 ? (
+          <p>Sorry! No seating times available at this date</p>
+        ) : (
+          <form>
+            <h2>Available seating times:</h2>
 
             <label>
-              <input type="radio" value="secondSeating" name="chosenSeating"
-               onChange={this.props.handleChange} />
+              <input
+                type="radio"
+                value="firstSeating"
+                name="chosenSeating"
+                onChange={this.props.handleChange}
+              />
+              18:00
+            </label>
+
+            <label>
+              <input
+                type="radio"
+                value="secondSeating"
+                name="chosenSeating"
+                onChange={this.props.handleChange}
+              />
               21:00
             </label>
-          
-          <button type="button" onClick={this.props.handleClick}>Proceed booking</button>
-        
-        </form>
+
+            <button type="button" onClick={this.props.handleClick}>
+              Proceed booking
+            </button>
+          </form>
         )}
       </React.Fragment>
     );
   }
- 
 }
