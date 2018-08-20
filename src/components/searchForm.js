0d7e068021b8aa314951b52d43958f4eb502b540
Caroline Wood <carowood@gmail.com>
2018-08-20 10:35:03 +0200

    Change file name to uppercase

--- src/components/searchForm.js
+++ src/components/searchForm.js
@@ -1,20 +1,28 @@
-import React from 'react';
+import React from "react";
 
-export default function searchForm({handleClick, dateValue, dateChange}){
+export default function searchForm({ handleClick, dateValue, dateChange }) {
+  let numberOfPeople = [
+    "1 person",
+    "2 people",
+    "3 people",
+    "4 people",
+    "5 people",
+    "6 people"
+  ];
 
-
-  let numberOfPeople = ["1 person", "2 people", "3 people" ,"4 people", "5 people", "6 people"];
-  
   return (
-  
-  <form>
-    <select name="numberOfPeople">
-    {numberOfPeople.map((people) => <option key={people}>{people}</option> ) }
-    </select>
-    <label htmlFor="date">Choose a date:</label>
-    
-    <input type="date" name="date" value={dateValue} onChange={dateChange}/>
-    <button type="button" onClick={handleClick} >Search</button>
-  </form>
+    <form>
+      <select name="numberOfPeople">
+        {numberOfPeople.map(people => (
+          <option key={people}>{people}</option>
+        ))}
+      </select>
+      <label htmlFor="date">Choose a date:</label>
+
+      <input type="date" name="date" value={dateValue} onChange={dateChange} />
+      <button type="button" onClick={handleClick}>
+        Search
+      </button>
+    </form>
   );
 }
