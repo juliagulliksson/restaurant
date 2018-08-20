39da118421fc6b59730ea530efcd5848f661b1a0
juliagulliksson <julia.gulliksson@medieinstitutet.se>
2018-08-20 09:52:39 +0200

    Booking process almost done

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
 import SearchForm from './SearchForm';
+import SeatingForm from './SeatingForm';
+import UserForm from './UserForm';
 
 class Book extends Component {
 
   state = {
     date: "",
     chooseSeating: false,
-    username: "",
+    name: "",
     email: "",
     phone: "",
-    seatingOne: "",
-    seatingTwo: ""
+    seatingTimes: {
+      seatingOne: "",
+      seatingTwo: ""
+    },
+    chosenSeating: "",
+    booking: false
   }
 
   handleChange = (event) => {
@@ -35,21 +41,52 @@ class Book extends Component {
       })
   }
 
-  setSeating = (seatingTimes) => {
+  setSeating = (response) => {
     this.setState({
-      seatingOne: seatingTimes.seatingOne,
-      seatingTwo: seatingTimes.seatingTwo,
+      seatingTimes: {
+        seatingOne: response.seatingOne,
+        seatingTwo: response.seatingTwo
+      },
       chooseSeating: true
     })
   }
 
+  proceedBooking = () => {
+    this.setState({
+      booking: true
+    })
+  }
+
+  book = () => {
+    console.log(this.state);
+   /*  let formValues = JSON.stringify(this.state);
+    fetch('http://localhost/restaurant/src/components/php/search.php?formData=' + formValues,{
+          method: 'GET',
+          headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json'
+          }
+      })
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response);
+        this.setSeating(response);
+      }) */
+  }
+
   render() {
     return (
-      <div className="App">
+      <div>
         <SearchForm dateChange= {this.handleChange}
         dateValue = {this.state.date} handleClick={this.searchForVacantSeatings}/>
 
-        {this.state.chooseSeating && <div><p>Hej</p></div>}
+        {this.state.chooseSeating && 
+        <SeatingForm seatingTimes = {this.state.seatingTimes}
+        chosenSeating = {this.state.chosenSeating}
+        handleChange={this.handleChange}
+        handleClick = {this.proceedBooking} /> }
+
+        {this.state.booking && <UserForm handleChange={this.handleChange} handleClick={this.book}/>}
       </div>
     );
   }
--- src/components/SeatingForm.js
+++ src/components/SeatingForm.js
@@ -1,26 +1,47 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-export default function seatingForm({handleClick, seatingOne, seatingTwo}){
+export default class SeatingForm extends Component {
 
-
-  //Determine which seatings are available
-  if(seatingOne < 15 && seatingTwo < 15){
-    message = "Both";
-   
-  } else if(seatingOne < 15) {
-  
-    message = "18:00";
-  } else if(seatingTwo < 15){
-   message = "21:00";
-  } else {
-    message = "None";
+  state = {
+    firstSeating: false,
+    secondSeating: false
   }
 
-  return (
-  <form>
-    <label htmlFor="date">Choose a date:</label>
-    <input type="date" name="date" value={dateValue} onChange={dateChange}/>
-    <button type="button" onClick={handleClick} >Search</button>
-  </form>
-  );
+  handleClick = () => {
+    this.setState((prevState) => ({
+      firstSeating: true
+    }));
+  }
+
+  render(){
+    return (
+      <React.Fragment>
+        { this.props.seatingTimes.seatingTwo > 14 && 
+        this.props.seatingTimes.seatingTwo > 14 ? 
+        <p>Sorry! No seating times available at this date</p>
+        : (
+      
+        <form>
+          <h2>Available seating times:</h2>
+ 
+             <label>
+              <input type="radio" value="firstSeating" name="chosenSeating"
+               onChange={this.props.handleChange} />
+              18:00
+            </label> 
+
+            <label>
+              <input type="radio" value="secondSeating" name="chosenSeating"
+               onChange={this.props.handleChange} />
+              21:00
+            </label>
+          }
+          <button type="button" onClick={this.props.handleClick}>Proceed booking</button>
+        
+        </form>
+        )}
+      </React.Fragment>
+    );
+  }
+ 
 }
