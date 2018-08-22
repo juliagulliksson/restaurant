2fa96d09eab3f1c9f4a7ce489816be69c4112a55
Caroline Wood <carowood@gmail.com>
2018-08-22 10:23:49 +0200

    Changed content-type to text/plain for POST request

--- src/components/Book.js
+++ src/components/Book.js
@@ -2,11 +2,11 @@ import React, { Component } from "react";
 import SearchForm from "./SearchForm";
 import SeatingForm from "./SeatingForm";
 import UserForm from "./UserForm";
-import moment from 'moment';
+import moment from "moment";
 
 class Book extends Component {
   state = {
-    date: moment().format('YYYY-MM-DD'),
+    date: moment().format("YYYY-MM-DD"),
     chooseSeating: false,
     name: "",
     email: "",
@@ -20,14 +20,14 @@ class Book extends Component {
     bookingComplete: false
   };
 
-  handleChange = (event) => {
+  handleChange = event => {
     //Update all input field states based on their HTML names
     this.setState({ [event.target.name]: event.target.value });
   };
 
-  handleDateChange = (date) => {
-    const selectedDate = date.format('YYYY-MM-DD');
-    this.setState({ date: selectedDate })
+  handleDateChange = date => {
+    const selectedDate = date.format("YYYY-MM-DD");
+    this.setState({ date: selectedDate });
   };
 
   searchForVacantSeatings = () => {
@@ -49,7 +49,7 @@ class Book extends Component {
       });
   };
 
-  setSeating = (response) => {
+  setSeating = response => {
     this.setState({
       seatingTimes: {
         seatingOne: response.seatingOne,
@@ -67,8 +67,8 @@ class Book extends Component {
     this.setState({
       booking: false,
       chooseSeating: false
-    })
-  }
+    });
+  };
 
   book = () => {
     console.log(this.state);
@@ -80,55 +80,60 @@ class Book extends Component {
         method: "GET",
         headers: {
           Accept: "application/json",
-          "Content-Type": "application/json"
+          "Content-Type": "text/plain"
         }
       }
-    )
-      .then(() => {
-        this.setState({
-          bookingComplete: true,
-          booking: false,
-          chooseSeating: false})
+    ).then(() => {
+      this.setState({
+        bookingComplete: true,
+        booking: false,
+        chooseSeating: false
       });
+    });
   };
 
   render() {
     let seating;
-    this.state.chosenSeating === "firstSeating" ? seating = "18:00" : seating = "21:00";
-    
+    this.state.chosenSeating === "firstSeating"
+      ? (seating = "18:00")
+      : (seating = "21:00");
+
     return (
       <div>
         <React.Fragment>
-        {this.state.bookingComplete ? (
-        
-        <div className="success">
-          <p>Congratulations! You have booked a table at {this.state.date} {seating}</p>
-        </div>  )
-        :
-        (
-         <SearchForm dateChange= {this.handleDateChange}
-                    dateValue = {this.state.date} 
-                    handleClick = {this.searchForVacantSeatings} />  )}
-        
-        
-        {this.state.chooseSeating && 
-        <SeatingForm  seatingTimes = {this.state.seatingTimes}
-                      chosenSeating = {this.state.chosenSeating}
-                      handleChange={this.handleChange}
-                      handleClick = {this.proceedBooking} /> }
+          {this.state.bookingComplete ? (
+            <div className="success">
+              <p>
+                Congratulations! You have booked a table at {this.state.date}{" "}
+                {seating}
+              </p>
+            </div>
+          ) : (
+            <SearchForm
+              dateChange={this.handleDateChange}
+              dateValue={this.state.date}
+              handleClick={this.searchForVacantSeatings}
+            />
+          )}
 
-        {this.state.booking && 
-        <UserForm handleChange={this.handleChange} 
-                  handleClick={this.book}
-                  handleCancel={this.cancelBooking}/>}
-        
-        
-      
-       
+          {this.state.chooseSeating && (
+            <SeatingForm
+              seatingTimes={this.state.seatingTimes}
+              chosenSeating={this.state.chosenSeating}
+              handleChange={this.handleChange}
+              handleClick={this.proceedBooking}
+            />
+          )}
+
+          {this.state.booking && (
+            <UserForm
+              handleChange={this.handleChange}
+              handleClick={this.book}
+              handleCancel={this.cancelBooking}
+            />
+          )}
         </React.Fragment>
       </div>
-        
-        
     );
   }
 }
