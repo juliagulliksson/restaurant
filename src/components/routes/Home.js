f5a940cf4e951be9b8ecbba346f2ecb7a3d6a580
ariviegea <ariadna.villar.egea@gmail.com>
2018-08-22 12:27:26 +0200

    footer + style

--- src/components/routes/Home.js
+++ src/components/routes/Home.js
@@ -5,6 +5,16 @@ export default function Home() {
     <div>
       <h1>Home</h1>
       <p>This is the home page.</p>
+    <main>
+    <div className="jumbotron">
     </div>
+    </main>
+    <footer>
+      <ul>
+        <li>Sturegatan 6, Humlegårdsgatan 19 • Karta • Tel +46 8 51977800 •</li>
+        <li>info@tavernabrillo.se • Vår integritetspolicy</li>
+      </ul>
+    </footer>
+    </div> 
   );
 }
