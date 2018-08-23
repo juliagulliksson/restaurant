a239129673ca6d13920029a7bd65ce9c613017ba
Caroline Wood <carowood@gmail.com>
2018-08-23 22:19:49 +0200

    Add search for booking by name

---
+++ src/components/BookingRow.js
@@ -0,0 +1,24 @@
+import React from "react";
+//import ButtonDelete from "./ButtonDelete";
+//import ButtonEdit from "./ButtonEdit";
+
+class BookingRow extends React.Component {
+  render() {
+    const booking = this.props.booking;
+
+    return (
+      <tr>
+        <td>{booking.bookingId}</td>
+        <td>{booking.date}</td>
+        <td>{booking.seatingOne == 1 ? "18:00" : "21:00"}</td>
+        <td>{booking.name}</td>
+        <td>{booking.userPhone}</td>
+        <td>{booking.email}</td>
+        <td>{/*<ButtonDelete />*/}</td>
+        <td>{/*<ButtonEdit />*/}</td>
+      </tr>
+    );
+  }
+}
+
+export default BookingRow;
