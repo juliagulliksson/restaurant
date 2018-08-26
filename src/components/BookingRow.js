0e594533b65712ee7c010c7f3bc84af9ddd62768
Caroline Wood <carowood@gmail.com>
2018-08-26 16:16:26 +0200

    Add delete modal confirmation

--- src/components/BookingRow.js
+++ src/components/BookingRow.js
@@ -1,19 +1,29 @@
 import React from "react";
+import DeleteBookingModal from "./DeleteBookingModal";
 
 class BookingRow extends React.Component {
   constructor(props) {
     super(props);
+    this.state = { show: false };
+
     this.handleClick = this.handleClick.bind(this);
   }
 
+  showModal = () => {
+    this.setState({ show: true });
+  };
+
+  hideModal = () => {
+    this.setState({ show: false });
+  };
+
   handleClick = () => {
-    console.log("click");
+    this.setState({ show: false });
     this.props.deleteBooking(this.props.booking.phone);
   };
 
   render() {
     const booking = this.props.booking;
-    //const phone = this.props.phone;
 
     return (
       <tr>
@@ -24,17 +34,22 @@ class BookingRow extends React.Component {
         <td>{booking.userPhone}</td>
         <td>{booking.email}</td>
         <td>
-          <button
-            /*handleClick={this.agreeGdpr}*/ className="btn btn-secondary"
-          >
-            Edit
-          </button>
+          <button className="btn btn-secondary">Edit</button>
         </td>
         <td>
-          <button
-            onClick={() => this.props.deleteBooking(booking.userPhone)}
-            className="btn btn-danger"
+          <DeleteBookingModal
+            show={this.state.show}
+            handleClose={this.hideModal}
+            handleConfirm={this.handleClick}
           >
+            <p>Booking ID: {booking.bookingId}</p>
+            <p>Date: {booking.date}</p>
+            <p>Seating time: {booking.seatingOne == 1 ? "18:00" : "21:00"}</p>
+            <p>Name: {booking.name}</p>
+            <p>Phone: {booking.userPhone}</p>
+            <p>Email: {booking.email}</p>
+          </DeleteBookingModal>
+          <button onClick={this.showModal} className="btn btn-danger">
             Delete
           </button>
         </td>
