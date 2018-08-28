9094d6b7f4a531df4f8d665dc876a6e6821f40f9
Caroline Wood <carowood@gmail.com>
2018-08-28 13:24:06 +0200

    add mailto function to sendMailConfirmation

--- src/components/php/sendConfirmationEmail.php
+++ src/components/php/sendConfirmationEmail.php
@@ -12,7 +12,11 @@ $data = json_decode($_GET["data"], false);
 // Query returns bookings for current date and future dates.
 // No past dates will be returned. Do we want to look at old bookings?
 
+$subject = "Reservation for ". $data->date ." has been cancelled";
+$message = "Dear " . $data->name . ", Your reservation for " . $data->date . 
+" at " . $data->seatingTime . " has been cancelled. Visit www.finnedinne.com to book again.";
 
+mail($data->email, $subject, $message);
 
 echo json_encode($data, JSON_PRETTY_PRINT);
 
