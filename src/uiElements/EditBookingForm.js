8b3d029ecf80afbce0466fa1cb9c78b266e7d29c
juliagulliksson <julia.gulliksson@medieinstitutet.se>
2018-09-04 12:49:39 +0200

    minor fix

--- src/uiElements/EditBookingForm.js
+++ src/uiElements/EditBookingForm.js
@@ -3,38 +3,38 @@ import React from 'react';
 export default function EditBookingForm(props){
   return(
       <div className="form-group">
-      <label className="editField" htmlFor="name">
-      Name
-      </label>
-      <input
-        className="editField form-control"
-        type="text"
-        name="name"
-        defaultValue={props.defaultName}
-        onChange={props.handleChange}
-      />
-      <br />
-      <label className="editField" htmlFor="phone">
-        Phone
-      </label>
-      <input
-        className="editField form-control"
-        type="text"
-        name="phone"
-        defaultValue={props.defaultPhone}
-        onChange={props.handleChange}
-      />
-      <br />
-      <label className="editField" htmlFor="email">
-        Email
-      </label>
-      <input
-        className="editField form-control"
-        type="text"
-        name="email"
-        defaultValue={props.defaultEmail}
-        onChange={props.handleChange}
-      />
+        <label className="editField" htmlFor="name">
+        Name
+        </label>
+        <input
+          className="editField form-control"
+          type="text"
+          name="name"
+          defaultValue={props.defaultName}
+          onChange={props.handleChange}
+        />
+        <br />
+        <label className="editField" htmlFor="phone">
+          Phone
+        </label>
+        <input
+          className="editField form-control"
+          type="text"
+          name="phone"
+          defaultValue={props.defaultPhone}
+          onChange={props.handleChange}
+        />
+        <br />
+        <label className="editField" htmlFor="email">
+          Email
+        </label>
+        <input
+          className="editField form-control"
+          type="text"
+          name="email"
+          defaultValue={props.defaultEmail}
+          onChange={props.handleChange}
+        />
       </div>
   )
 }
